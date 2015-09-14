a9baaf62295404b5a76cccc7b3e509c8d72997d8
= <=>
2015-09-14 22:27:49 +0800

    add stringize info.
    Signed-off-by: = <=>

--- pycode/enum.py
+++ pycode/enum.py
@@ -2,6 +2,7 @@
 #-*-: coding: utf-8 -*-
 
 # author : zergl <e3.gemini@qq.com>
+#   site : https://github.com/zergl
 #   date : 2015/08/20
 #
 
@@ -9,16 +10,22 @@ def enum(args, start=0):
     class _Enum(object):
         ##todo: make sure every element is a valid variable.
         __slots__ = args.split(',')
+        __item_str = {}
 
         def __init__(self):
             for idx, key in enumerate(_Enum.__slots__, start):
+                key = key.strip()
                 setattr(self, key, idx)
+                self.__item_str[idx] = key
         
-        def __str__(self):
-            return "An enum-like wrapper."
+        def __str__(self, key):
+            return "An Enum-like Wrapper."
 
         def __iter__(self):
             return iter(self.__slots__)
+        
+        def __getitem__(self, key):
+            return getattr(self, key)
 
         def __len__(self):
             return len(_Enum.__slots__)
@@ -26,15 +33,26 @@ def enum(args, start=0):
         def append(self, key, value):
             idx = len(_Enum.__slots__)
             setattr(self, key, )
+    
+        def str(self, key):
+            return self.__item_str[key]
+
     return _Enum()
 
 if __name__ == "__main__":
-    e = enum('a,b,c,d')
-    print "len(enum): ", len(e)
+    e = enum('BUDEG,INFO,ALERT,CRITI')
+    print "value of e.INFO: ", e.INFO
+    print "string value of e.INFO: ", e.str(e.INFO)
+
+    print "len(e): ", len(e)
     #print "e[1]: ", e[1] ## indexing -- not support
     #e = enum() ## callable -- not support
     #e.append(abcd) ## append -- not support
 
+    for k in e:
+        print "%s - %s " %(k, e[k])
+
+    print "--------"
     for i,k in enumerate(e):
         print "%d - %s" %(i, k)
 
